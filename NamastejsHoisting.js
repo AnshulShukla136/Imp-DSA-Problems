6702c4d796c60a4fd0c80d35ddf126b8042e2083
AnshulShukla136 <anshulshukla136@gmail.com>
2026-05-09 14:27:43 +0530

    dpp 8/5/2026

---
+++ NamastejsHoisting.js
@@ -0,0 +1,9 @@
+
+var x = 5;
+function getName() {
+    console.log("my name is anshul");
+}
+console.log(x)
+console.log(getName);
+// console.log(getName());
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+var x = 5;
+function getName() {
+    console.log("my name is anshul");
+}
+console.log(x)
+console.log(getName);
