cc6e5943f2a8f3d23f919adbe9afbd2279f0d446
AnshulShukla136 <anshulshukla136@gmail.com>
2026-05-11 09:41:49 +0530

    11/05/2026 dpp

---
+++ LeetcodeDPP/Easy.cpp/1.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+    vector<int> separateDigits(vector<int> &nums)
+    {
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int n = nums[i];
+            vector<int> temp;
+            while (n > 0)
+            {
+                int rem = n % 10;
+                temp.push_back(rem);
+
+                n = n / 10;
+            }
+            int len = temp.size();
+            for (int j = len - 1; j >= 0; j--)
+            {
+                ans.push_back(temp[j]);
+            }
+        }
+        return ans;
+    }
+};
--- LeetcodeDPP/dp/2770.cpp
+++ LeetcodeDPP/dp/2770.cpp
@@ -1,28 +1,21 @@
-class Solution
-{
+class Solution {
 public:
-    int solve(vector<int> &nums, int target, int i, vector<int> &dp)
-    {
-
-        if (i == nums.size() - 1)
-            return dp[i] = 0;
-        if (dp[i] != -1)
-            return dp[i];
+    int solve(vector<int>& nums, int target, int i, vector<int>&dp) {
+        
+        if(i == nums.size()-1)return dp[i] = 0;
+        if(dp[i] != -1)return dp[i];
         int res = INT_MIN;
-        for (int j = i + 1; j < nums.size(); j++)
-        { // from current index forward we have choice in which we can go so a for loop
-            if (abs(nums[i] - nums[j]) <= target)
-            {
-                int take = 1 + solve(nums, target, j, dp);
+        for(int j = i+1; j < nums.size(); j++){         //from current index forward we have choice in which we can go so a for loop
+            if(abs(nums[i] - nums[j]) <= target){
+                int take = 1 + solve(nums,target,j, dp);
                 res = max(res, take);
             }
         }
         return dp[i] = res;
     }
-    int maximumJumps(vector<int> &nums, int target)
-    {
+    int maximumJumps(vector<int>& nums, int target) {
         int n = nums.size();
-        vector<int> dp(n + 1, -1);
+        vector<int>dp(n+1, -1);
         int ans = solve(nums, target, 0, dp);
         return ans < 0 ? -1 : ans;
     }
