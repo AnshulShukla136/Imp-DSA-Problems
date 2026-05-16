73c207efb3e9887de7c1327e7bde60b32c21c83c
AnshulShukla136 <anshulshukla136@gmail.com>
2026-05-16 12:33:21 +0530

    minimum in rotated sorted array 2

---
+++ LeetcodeDPP/Binary Search/Minimum in rotated sorted array.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+    int findMin(vector<int> &nums)
+    {
+        int n = nums.size();
+        if (n == 1)
+            return nums[0];
+        int l = 0;
+        int r = n - 1;
+        int ans = -1;
+
+        while (l <= r)
+        {
+            int mid = l + (r - l) / 2;
+
+            if (mid - 1 >= 0 && nums[mid] < nums[mid - 1] && mid + 1 < n && nums[mid] < nums[mid + 1] || mid - 1 < 0 && nums[mid] < nums[mid + 1] || mid + 1 >= n && nums[mid] < nums[mid - 1])
+            {
+                ans = nums[mid];
+                return nums[mid];
+            }
+
+            if (nums[mid] > nums[r])
+            {
+                l = mid + 1;
+            }
+            if (nums[mid] < nums[r])
+            {
+                r = mid;
+            }
+        }
+        return ans;
+    }
+};
