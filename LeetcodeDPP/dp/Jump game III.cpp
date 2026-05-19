e124cb29abfd715b91c96e0a3e0af40b92b752ce
AnshulShukla136 <anshulshukla136@gmail.com>
2026-05-19 09:51:47 +0530

    minimum common value

--- LeetcodeDPP/dp/Jump game III.cpp
+++ LeetcodeDPP/dp/Jump game III.cpp
@@ -1,28 +1,22 @@
-// No memoisation we have made 1d visited array just like dfs
+// No memoisation we have made 1d visited array just like dfs 
 // TC = O(n)
-// SC = O(n)
+//SC = O(n) 
 
-class Solution
-{
+class Solution {
 public:
-    bool solve(vector<int> &nums, int i, vector<bool> &vis)
-    {
-        if (i < 0 || i >= nums.size())
-            return false;
-
-        if (nums[i] == 0)
-            return true;
-        if (vis[i])
-            return false;
+    bool solve(vector<int>& nums, int i,vector<bool>&vis){
+        if(i < 0 || i >= nums.size())return false;
+        
+        if(nums[i] == 0)return true;
+        if(vis[i])return false; 
         vis[i] = true;
         bool first = solve(nums, i + nums[i], vis);
-        bool second = solve(nums, i - nums[i], vis);
+        bool second = solve(nums, i - nums[i],vis);
 
         return first || second;
     }
-    bool canReach(vector<int> &nums, int start)
-    {
-        vector<bool> vis(nums.size(), false);
-        return solve(nums, start, vis);
+    bool canReach(vector<int>& nums, int start) {
+        vector<bool>vis(nums.size(), false);
+        return solve(nums, start,vis);
     }
 };
