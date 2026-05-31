d19dc28867932f9568862e03bcb98776d59667ea
AnshulShukla136 <anshulshukla136@gmail.com>
2026-05-31 21:47:25 +0530

    added another method

--- LeetcodeDPP/Greedy/2126. Destroying Asteroids.cpp
+++ LeetcodeDPP/Greedy/2126. Destroying Asteroids.cpp
@@ -16,3 +16,28 @@ public:
         return true;
     }
 };
+
+// TC = O(nlogn)
+// SC = O(n)
+
+class Solution
+{
+public:
+    bool asteroidsDestroyed(int mass, vector<int> &ast)
+    {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        long long m = mass;
+        for (int i = 0; i < ast.size(); i++)
+        {
+            pq.push(ast[i]);
+        }
+        while (!pq.empty())
+        {
+            if (pq.top() > m)
+                return false;
+            m += pq.top();
+            pq.pop();
+        }
+        return true;
+    }
+};
