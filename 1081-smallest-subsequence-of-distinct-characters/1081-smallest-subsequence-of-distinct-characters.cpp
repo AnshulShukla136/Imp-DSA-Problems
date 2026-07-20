f3d26f236f3b9ec2c64408a393eec12f3d973dd0
Anshul Shukla <152410688+AnshulShukla136@users.noreply.github.com>
2026-07-20 11:30:17 +0530

    Time: 0 ms (100%), Space: 8.6 MB (93.99%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     string smallestSubsequence(string s) {
-        int freq[27] = {0};
-        bitset<27> seen;
-        string stack;
+        int n = s.size();
+        string result = "";
+        vector<bool>taken(26, false);
+        vector<int>lastIdx(26);
 
-        for (auto& c : s)
-            freq[c & 31]++;
+        for(int i = 0; i < n; i++){
+            char ch = s[i];
 
-        for (auto& c : s) {
-            int x = c & 31;
-            freq[x]--;
-
-            if (seen.test(x))
-                continue;
-
-            while (stack.length() && stack.back() > c && freq[stack.back() & 31]) {
-                seen.reset(stack.back() & 31);
-                stack.pop_back();
-            }
-
-            stack.push_back(c);
-            seen.set(x);
+            lastIdx[ch - 'a'] = i;
         }
 
-        return stack;
+        for(int i = 0; i < n; i++){
+            char ch = s[i];
+            
+            if(taken[ch - 'a']){
+                continue;
+            }
+            while(result.size() > 0 && result.back() > ch && lastIdx[result.back() - 'a'] > i){
+                taken[result.back() - 'a'] = false;
+                result.pop_back();
+            }
+            result.push_back(ch);
+            taken[ch - 'a'] = true;
+        }
+        return result;
     }
 };
